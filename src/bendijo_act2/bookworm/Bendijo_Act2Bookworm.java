bfd20398199641a4e19684b45137180f0aab4a54
Bendijo <Bendijo@Bendijo.portal.pisofiph.com>
2023-09-16 21:32:20 +0800

    Activity1 - FinalFinal

--- src/bendijo_act2/bookworm/Bendijo_Act2Bookworm.java
+++ src/bendijo_act2/bookworm/Bendijo_Act2Bookworm.java
@@ -29,15 +29,6 @@ public Node insertNode (String author, String title, String ISBN){
     return head;
     }        
 
-public void display(){
-         Node node = head;
-            while (node != null){
-                System.out.println("Author: " + node.author + ", Title: " + node.title + ", ISBN: " + node.ISBN);
-                node = node.next;
-     }
-         System.out.println();
-     }
-
 public void searchAuthor(String targetAuthor){
         Node current = head;
         
@@ -46,16 +37,16 @@ public void searchAuthor(String targetAuthor){
         while (current != null ) {
             if (current.author.equalsIgnoreCase(targetAuthor)) {
                 found = true;
-                System.out.println("\nThe author" + "'" + current.author + "'" + " is FOUND in the BOOK: \n");
-                System.out.println("Author: " + current.author + ", Title: " + current.title + ", ISBN: " + current.ISBN + "\n" );
-                System.out.println();
+                System.out.println("\nThe author " + "'" + current.author + "'" + " is FOUND in the BOOK: \n==========DETAILS==========\n");
+                System.out.println("♥ Author: " + current.author + ", Title: " + current.title + ", ISBN: " + current.ISBN + "\n" );
+                current = current.next;
             }
-            current = current.next;
         }
-        if (!found) {
-            System.out.println("Author is UNKNOWN");
+                if(!found){
+                System.out.println("Author is UNKNOWN");
+                }
         }
-    }
+    
 
 public void searchTitle(String targetTitle) {
         Node current = head;
@@ -64,16 +55,15 @@ public void searchTitle(String targetTitle) {
         while (current != null) {
             if (current.title.equalsIgnoreCase(targetTitle)) {
                 found = true;
-                System.out.println("\nBook " + "'" + current.title + "'" + " is FOUND \n");
-                System.out.println("Author: " + current.author + ", Title: " + current.title + ", ISBN: " + current.ISBN + "\n" );
-                System.out.println();
+                System.out.println("\nBook " + "'" + current.title + "'" + " is FOUND \n==========DETAILS==========\n");
+                System.out.println("♥ Author: " + current.author + ", Title: " + current.title + ", ISBN: " + current.ISBN + "\n" );
+                current = current.next;
             }
-            current = current.next;
         }
-        if (!found) {
-            System.out.println("THERES NO SUCH TITLE OF THE BOOK");
+        if (!found){
+                System.out.println("THERES NO SUCH TITLE OF THE BOOK");
         }
-    }
+       }
 
 public void searchIsbn(String targetIsbn) {
         Node current = head;
@@ -82,48 +72,52 @@ public void searchIsbn(String targetIsbn) {
         while (current != null) {
             if (current.ISBN.equalsIgnoreCase(targetIsbn)) {
                 found = true;
-                System.out.println("\nBook " + "'" + current.ISBN + "'" + " is FOUND \n");
-                System.out.println("Author: " + current.author + ", Title: " + current.title + ", ISBN: " + current.ISBN + "\n" );
-                System.out.println();
+                System.out.println("\nThe ISBN " + "'" + current.ISBN + "'" + " exist \n ==========DETAILS==========");
+                System.out.println("♥ Author: " + current.author + ", Title: " + current.title + ", ISBN: " + current.ISBN + "\n");
+                current = current.next;
             }
-            current = current.next;
         }
-
-        if (!found) {
+                if (!found){
             System.out.println("ISBN is not FOUND");
-        }
-    }
-   
+            }
+}
+
+   public void display(){
+    Node node = head;
+        while (node != null){
+                System.out.println("♥ Author: " + node.author + ", Title: " + node.title + ", ISBN: " + node.ISBN);
+                node = node.next;
+     }
+         System.out.println();
+     }
 public static void main(String[] args) throws Exception{
         Bendijo_Act2Bookworm main = new Bendijo_Act2Bookworm ();
         
         Scanner scan = new Scanner (System.in);
         boolean start = true;
         
-
         while(start){
             
         System.out.println("==========MENU=========");
         System.out.println("[1] Add a Book");
         System.out.println("[2] Search a Book");
         System.out.println("[3] Display Library\n");
-        int menuInput = scan.nextInt();
         
-            switch (menuInput) {
-                case 1:
+        
+        int menuInput = scan.nextInt();
+        if (menuInput == 3){
+            main.display();
+        }else if (menuInput == 1) {
                     System.out.println("Author's name: ");
                     String addauthor = scan.next();
                     System.out.println("Book title: ");
                     String addtitle = scan.next();
                     System.out.println("Enter the ISBN");
                     String addISBN = scan.next();
-                    System.out.println("\n\nNew Added Book Details: \nAuthor's Name: " + addauthor + "\nTitle: " + addtitle + "\nISBN: " + addISBN + "\n");
-                    main.insertNode(addauthor, addtitle, addISBN );
-                    break;
-
-                default:
                     
-                case 2:                    
+                    System.out.println("\nNew Added Book Details: \nAuthor's Name: " + addauthor + "\nTitle: " + addtitle + "\nISBN: " + addISBN + "\n");
+                    main.insertNode(addauthor, addtitle, addISBN );
+        }else if (menuInput == 2){                  
                         System.out.println("====Search=====");
                         System.out.println("[1] Author");
                         System.out.println("[2] Title");
@@ -153,16 +147,16 @@ public static void main(String[] args) throws Exception{
                                 String searchISBN = scan.nextLine();
                                 main.searchIsbn(searchISBN);
                                 break;
-                                
+
                             default:
                         }
-                case 3: 
-                    main.display();
-                    break;
-                case 4: 
-                    throw new Exception("Invalid input");
-                
+//                case 4: 
+//                    throw new Exception("Invalid input");
+//                
                         }
+        else {
+            throw new Exception("Invalid Choice");
+        }
                     }
             }
     }
